f03e852a4c8c8c11a237e6230ec263849ff7725b
Kenzo Mendoza <kenzom954@gmail.com>
2016-08-05 14:12:05 -0700

    removed gulp

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path')
+var port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+})
+
+app.listen(port,function(){
+  console.log('listening port',port)
+})
---
+++ public/javascript/search.js
@@ -0,0 +1,8 @@
+$("#search-btn").click(function(){
+  console.log('this is search button')
+})
+
+
+$("#history-btn").click(function(){
+  console.log('this is history button')
+})
