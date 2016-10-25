a9eb67d3fda5136b469ab051295771790a55893e
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-24 21:42:50 -0700

    updated styling

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,27 +1,29 @@
+"use strict"
 $("#search-btn").click(function(e){
   e.preventDefault();
   var search = $("#search").val();
   var count = $("#count").val();
   var offset = $("#offset").val();
   $.ajax({
-    url: '/api/imagesearch',
-    type: 'POST',
-    dataType: 'JSON',
+    url: "/api/imagesearch",
+    type: "POST",
+    dataType: "JSON",
     beforeSend: function(){
       if(!Number.isInteger(Number(count)) || !Number.isInteger(Number(offset)) ){
-        console.log(Number.isInteger(Number(count)));
-        alert('Make sure Count and Offset Parameters are numbers!');
+        $("#hyperlink").empty();
+        $("#hyperlink").append("<h3>Make sure Count and Offset are numbers</h3>");
         return false;
       }
-      if(search === null || count === null || count === offset){
-        alert('Make sure none are empty!');
-        return false
+      if(search === "" || count === "" || offset === ""){
+        $("#hyperlink").empty();
+        $("#hyperlink").append("<h3>Fill out all the form</h3>");
+        return false;
       }
     },
     data: {query: search, count: count, offset: offset}, //data sending out to server is in JSON
     success: function(data){
       $("#hyperlink").empty();
-      $("#hyperlink").append('<h3><a href='+ data.link +'>'+ data.link +'</a></h3>')
+      $("#hyperlink").append("<h3><a href="+ data.link +">"+ data.link +"</a></h3>");
     }
   });
 })
@@ -29,11 +31,11 @@ $("#search-btn").click(function(e){
 
 $("#history-btn").click(function(){
   $.ajax({
-    url: '/api/gethistory',
-    type: 'GET',
+    url: "/api/gethistory",
+    type: "GET",
     success: function(data){
       $("#hyperlink").empty();
-      $("#hyperlink").append('<h3><a href='+ data.link +'>'+ data.link +'</a></h3>')
+      $("#hyperlink").append("<h3><a href="+ data.link +">"+ data.link +"</a></h3>")
     }
   })
 })
